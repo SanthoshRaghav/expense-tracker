3bfc4dfc002e5d08bb6a7b6fdf288c8ef96e88fe
Santhosh <santhosh@Santhoshs-MacBook-Pro.local>
2023-04-19 23:20:55 +0530

    Added tailwind to the project

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <h1>Expense Tracker</h1>
+      <h1 className='text-3xl font-bold underline'>Expense Tracker</h1>
     </div>
   );
 }
