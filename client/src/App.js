0172b91cca6c7d9ea0a791884844d3ebde635a6e
Santhosh <santhosh@Santhoshs-MacBook-Pro.local>
2023-04-19 23:33:47 +0530

    Added first test case and removed unnecessary css

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <h1 className='text-3xl font-bold underline'>Expense Tracker</h1>
+      <div className='container mx-auto max-w6xl text-center drop-shadow text-gray-800'>
+        <h1 className='text-4xl py-8 mb-10 bg-slate-800 text-white rounded'>Expense Tracker</h1>
+      </div>
     </div>
   );
 }
